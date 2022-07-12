41bfa794e4c7be9052c1d06f9301d7a414fc9b07
Bishoy <bishoy.samwuel@gmail.com>
2022-07-12 13:39:56 +0200

    Add Seach component, btn logic to hide it

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,23 @@
 import { useState } from 'react'
 import logo from './logo.svg'
 import './App.css'
+import Search from './Components/Search';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [showSearchPage, setShowSearchpage] = useState(false);
 
   return (
     <div className="App">
-      <header className="App-header">
-        <h1> Hello World </h1>
-      </header>
+        {showSearchPage ? (
+          <Search />
+        ) : (
+          <></>
+        )}
+      <div className="open-search">
+        <a onClick={() => setShowSearchpage(!showSearchPage)}>Add a book</a>
+      </div>
     </div>
+
   )
 }
 
