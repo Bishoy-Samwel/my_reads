546db51e2e7885d59035b1c07e9b8710ae7529bc
Bishoy <bishoy.samwuel@gmail.com>
2022-07-12 19:37:27 +0200

    Passing the sublist for each category

--- src/Components/Books.jsx
+++ src/Components/Books.jsx
@@ -1,14 +1,20 @@
 import Bookshelf from './Bookshelf';
-const Books = () => {
+import { useEffect } from 'react';
+const Books = (props) => {
+  let {books} = props
+  let currentlyReading = books.filter(book=>book.shelf=="currentlyReading")
+  let wantToRead = books.filter(book=>book.shelf=="wantToRead")
+  let read = books.filter(book=>book.shelf=="read")
+  
   return (<div className="list-books">
     <div className="list-books-title">
       <h1>MyReads</h1>
     </div>
     <div className="list-books-content">
       <div>
-        <Bookshelf category="currentlyReading"/>
-        <Bookshelf category="wantToRead"/>
-        <Bookshelf category="read"/>
+        <Bookshelf list={currentlyReading} category="currentlyReading"/>
+        <Bookshelf list={wantToRead} category="wantToRead"/>
+        <Bookshelf list={read} category="read"/>
       </div>
     </div>
   </div>)
