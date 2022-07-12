3de65179738e12c63bba429e1a2bcc07358be6c0
Bishoy <bishoy.samwuel@gmail.com>
2022-07-12 19:38:16 +0200

    Passing the book to the component

--- src/Components/Bookshelf.jsx
+++ src/Components/Bookshelf.jsx
@@ -1,14 +1,19 @@
 import Book from "./Book";
 
 const Bookshelf = (props) => {
-  const {category} = props;
+  const {category, list} = props;
   return (<div className="bookshelf">
     <h2 className="bookshelf-title">{category}</h2>
     <div className="bookshelf-books">
       <ol className="books-grid">
-        <li>
-          <Book category={category}/>
-        </li>
+        {
+          list.map(book=>{
+          return (<li>
+            <Book book={book} category={category}/>
+          </li>)
+          })
+        }
+       
       </ol>
     </div>
   </div>);
