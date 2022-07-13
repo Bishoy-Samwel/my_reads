e42a32087adc63b1901e2ffef7c02d8f4ac29b3e
Bishoy <bishoy.samwuel@gmail.com>
2022-07-13 15:31:40 +0200

    Add result state, onChange update state using seaarch fun

--- src/Components/Search.jsx
+++ src/Components/Search.jsx
@@ -1,8 +1,11 @@
 import Filtered from "./Filtered"
 import { Link } from 'react-router-dom';
+import { search } from "../BooksAPI";
+import { useState } from "react";
 
 const Search = (props) => {
   const {setShowSearchpage, showSearchPage} = props
+  const [result, setResult] = useState([])
   return (
     <div className="search-books">
           <div className="search-books-bar">
@@ -11,10 +14,11 @@ const Search = (props) => {
               <input
                 type="text"
                 placeholder="Search by title, author, or ISBN"
+                onChange={e => {search(e.target.value, 100).then(r=> setResult(r))}}
               />
             </div>
           </div>
-          <Filtered />
+          <Filtered result={result}/>
     </div>
   )
 }
