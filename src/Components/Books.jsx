7dc8ac87b660f0302c2a0cbed049ef133408fd58
Bishoy <bishoy.samwuel@gmail.com>
2022-07-13 12:32:30 +0200

    Add link to navigate to search

--- src/Components/Books.jsx
+++ src/Components/Books.jsx
@@ -1,20 +1,24 @@
 import Bookshelf from './Bookshelf';
 import { useEffect } from 'react';
+import { Link } from 'react-router-dom';
 const Books = (props) => {
-  let {books} = props
-  let currentlyReading = books.filter(book=>book.shelf=="currentlyReading")
-  let wantToRead = books.filter(book=>book.shelf=="wantToRead")
-  let read = books.filter(book=>book.shelf=="read")
-  
+  let { books } = props
+  let currentlyReading = books.filter(book => book.shelf == "currentlyReading")
+  let wantToRead = books.filter(book => book.shelf == "wantToRead")
+  let read = books.filter(book => book.shelf == "read")
+
   return (<div className="list-books">
     <div className="list-books-title">
       <h1>MyReads</h1>
     </div>
     <div className="list-books-content">
       <div>
-        <Bookshelf list={currentlyReading} category="currentlyReading"/>
-        <Bookshelf list={wantToRead} category="wantToRead"/>
-        <Bookshelf list={read} category="read"/>
+        <Bookshelf list={currentlyReading} category="currentlyReading" />
+        <Bookshelf list={wantToRead} category="wantToRead" />
+        <Bookshelf list={read} category="read" />
+        <div className="open-search">
+          <Link to="/seacrh">Add a book</Link>
+        </div>
       </div>
     </div>
   </div>)
