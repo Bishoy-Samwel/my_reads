e39fe255f4097d6968c04aaed3704e0850a9fb4c
Bishoy <bishoy.samwuel@gmail.com>
2022-07-13 12:32:46 +0200

    Add link to navigate to home

--- src/Components/Search.jsx
+++ src/Components/Search.jsx
@@ -1,16 +1,12 @@
 import Filtered from "./Filtered"
+import { Link } from 'react-router-dom';
 
 const Search = (props) => {
   const {setShowSearchpage, showSearchPage} = props
   return (
     <div className="search-books">
           <div className="search-books-bar">
-            <a
-              className="close-search"
-              onClick={() => {setShowSearchpage(!showSearchPage)}}
-            >
-              Close
-            </a>
+          <Link className="close-search" to="/">Add a book</Link>
             <div className="search-books-input-wrapper">
               <input
                 type="text"
