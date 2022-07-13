6315bc77a50b679d691f1b0119f80ee2839ff716
Bishoy <bishoy.samwuel@gmail.com>
2022-07-13 12:32:00 +0200

    Controle visibility by using Routes, Route

--- src/App.jsx
+++ src/App.jsx
@@ -4,31 +4,25 @@ import './App.css'
 import Search from './Components/Search';
 import Books from './Components/Books';
 import { getAll } from './BooksAPI';
+import { Link, Route, Routes } from 'react-router-dom';
 
 function App() {
   const [showSearchPage, setShowSearchpage] = useState(false);
   const [books, setBooks] = useState([]);
 
   useEffect(
-    ()=>{
-      getAll().then((books)=> setBooks(books))
-    },[books])
+    () => {
+      getAll().then((books) => setBooks(books))
+    }, [books])
 
   return (
     <div className="App">
-      {showSearchPage ? (
-        <Search setShowSearchpage={setShowSearchpage} showSearchPage={showSearchPage} />
-      ) : (
-        <>
-        <Books books={books} />
-          <div className="open-search">
-            <a onClick={() => setShowSearchpage(!showSearchPage)}>Add a book</a>
-          </div>
-        </>
-      )}
+      <Routes>
+        <Route path="/" element={<Books books={books} />} />
+        <Route path="seacrh" element={<Search setShowSearchpage={setShowSearchpage} showSearchPage={showSearchPage} />} />
+      </Routes>
 
     </div>
-
   )
 }
 
