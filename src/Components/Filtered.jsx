4eb6ab32ac4d6005c2a146a886f99afa76cbde83
Bishoy <bishoy.samwuel@gmail.com>
2022-07-14 12:56:46 +0200

    Show category for added books

--- src/Components/Filtered.jsx
+++ src/Components/Filtered.jsx
@@ -1,14 +1,17 @@
 import Book from './Book';
 const Filtered = (props) => {
-  const {result} =  props 
+  const { result, books } = props
   return (
     <div className="search-books-results">
       <ol className="books-grid">
         {
-          result&&result.length>0 && result.map((book, i) => {
-            return (
-         <Book key={i} book={book}/>
-        )
+          result && result.length > 0 && result.map((r, i) => {
+            const filteredBooks = books.filter(book => book.title === r.title)
+            if (filteredBooks.length === 1) {
+              return (<Book key={i} book={filteredBooks[0]} category={filteredBooks[0].shelf} />)
+            } else {
+              return (<Book key={i} book={r} />)
+            }
           })
         }
       </ol>
